68eb814c5014f0c3c3d32640f98eb2fd7e7a8ea2
Foenki <pierrot.lagier@wanadoo.fr>
2019-03-24 18:03:20 +0100

    Use express app

--- hello.js
+++ hello.js
@@ -8,7 +8,9 @@ var http = require('http')
 var url = require('url')
 var fs = require('fs')
 var path = require('path')
-var baseDirectory = '../'
+var express = require('express')
+var frontEndApp = express();
+var backEndApp = express();
 
 class Run
 {
@@ -53,32 +55,17 @@ MongoClient.connect(databaseURL, {useNewUrlParser:true}, function(err, database)
 
 function launchListening()
 {
-	http.createServer(handleHTTPRequest).listen(htmlPort)
-	http.createServer(handleRunsRequest).listen(jsonPort)
+	frontEndApp.get('/', handleHTTPRequest);
+	frontEndApp.use(express.static(path.resolve(__dirname,'public')));
+	frontEndApp.listen(htmlPort);
+	backEndApp.get('/', handleRunsRequest);
+	backEndApp.listen(jsonPort);
 	console.log('App running !')
 }
 
 function handleHTTPRequest(request, response)
 {
-	try {
-		var requestUrl = url.parse(request.url)
-
-		// need to use path.normalize so people can't access directories underneath baseDirectory
-		var fsPath = baseDirectory+path.normalize(requestUrl.pathname)
-		var fileStream = fs.createReadStream(fsPath)
-		fileStream.pipe(response)
-		fileStream.on('open', function() {
-				response.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'})
-		})
-		fileStream.on('error',function(e) {
-				 response.writeHead(404)     // assume the file doesn't exist
-				 response.end()
-		})
- } catch(e) {
-			response.writeHead(500)
-			response.end()     // end the response so browsers don't hang
-			console.log(e.stack)
- }
+	response.sendFile(path.join(__dirname + '/views/index.html'));
 }
 
 async function handleRunsRequest(request, response) 
